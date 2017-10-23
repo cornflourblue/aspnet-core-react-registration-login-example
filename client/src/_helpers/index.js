6c26907b3dce9142c9c709964c12c34faa29da0b
Jason Watmore <jason.watmore@tabcorp.com.au>
2017-10-23 16:35:49 +1100

    removed fake backend

--- client/src/_helpers/index.js
+++ client/src/_helpers/index.js
@@ -1,5 +1,4 @@
 export * from './auth-header';
 export * from './config';
-export * from './fake-backend';
 export * from './history';
 export * from './store';
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,10 +5,6 @@ import { Provider } from 'react-redux';
 import { store } from './_helpers';
 import { App } from './App';
 
-// setup fake backend
-// import { configureFakeBackend } from './_helpers';
-// configureFakeBackend();
-
 render(
     <Provider store={store}>
         <App />
